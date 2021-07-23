c168fa7d7ef2c512f6fcd0512b0c7e681fa4b750
notomo <notomo.motono@gmail.com>
2021-07-24 08:20:48 +0900

    Improve scroll restoring

--- lua/searcho/core/origin.lua
+++ lua/searcho/core/origin.lua
@@ -10,6 +10,9 @@ M.Origin = Origin
 function Origin.new(window_id)
   vim.validate({window_id = {window_id, "number"}})
 
+  local saved = vim.api.nvim_win_call(window_id, function()
+    return vim.fn.winsaveview()
+  end)
   local tbl = {
     position = vim.api.nvim_win_get_cursor(window_id),
     _window_id = window_id,
@@ -17,6 +20,7 @@ function Origin.new(window_id)
     _search_direction = SearchDirection.current(),
     _search_scroll = SearchScroll.current(window_id),
     _register = vim.fn.getreg("/"),
+    _saved = saved,
   }
   return setmetatable(tbl, Origin)
 end
@@ -25,6 +29,9 @@ function Origin.restore(self)
   vim.fn.setreg("/", self._register)
   self._search_direction:set()
   self:restore_scrolloff()
+  vim.api.nvim_win_call(self._window_id, function()
+    vim.fn.winrestview(self._saved)
+  end)
 
   -- +1 for stopinsert
   vim.api.nvim_win_set_cursor(self._window_id, {self.position[1], self.position[2] + 1})
