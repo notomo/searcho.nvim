e7c6542c78577bb9f9a4a00437f43cee662b5817
notomo <notomo.motono@gmail.com>
2021-07-31 10:11:56 +0900

    Fix search \v for multibyte

--- lua/searcho/core/search_result.lua
+++ lua/searcho/core/search_result.lua
@@ -115,14 +115,13 @@ end
 function SearchResultFactory._matched_end(self, row, col, input)
   local origin_row, origin_col = unpack(vim.api.nvim_win_get_cursor(self._window_id))
   cursorlib.left(self._window_id, row, col - 1) -- HACK: for `e` flag
+  if input == "\\v" then
+    input = "\\v."
+  end
   local end_row, end_col = unpack(vim.api.nvim_win_call(self._window_id, function()
     return vim.fn.searchpos(input, self._end_match_flag)
   end))
   vim.api.nvim_win_set_cursor(self._window_id, {origin_row, origin_col})
-
-  if input == "\\v" then -- HACK
-    end_col = math.max(end_col - 1, 1)
-  end
   return {end_row, end_col}
 end
 
