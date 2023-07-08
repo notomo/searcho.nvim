e93ca22e9bf8d6b28bdd5503153705ae8a467a4e
notomo <notomo.motono@gmail.com>
2023-07-08 14:54:15 +0900

    Add doc

--- lua/searcho/init.lua
+++ lua/searcho/init.lua
@@ -1,17 +1,31 @@
 local searcho = {}
 
+--- Setup keymap for command-line mode that type is search.
+--- @param keymap_func fun(vim:table) set keymap function. (The argument is patched vim object)
 function searcho.setup_keymaps(keymap_func)
-  return require("searcho.command").setup_keymaps(keymap_func)
+  require("searcho.command").setup_keymaps(keymap_func)
 end
 
+--- @class SearchoOption
+--- @field char_pattern string?  (default = "\k")
+
+--- Returns a command to start searching forward.
+--- @param opts SearchoOption?: |SearchoOption|
+--- @return string # command to use expr mapping
 function searcho.forward(opts)
   return require("searcho.command").forward(opts)
 end
 
+--- Returns a command to start searching backward.
+--- @param opts SearchoOption?: |SearchoOption|
+--- @return string # command to use expr mapping
 function searcho.backward(opts)
   return require("searcho.command").backward(opts)
 end
 
+--- Returns a command with the following side-effect.
+--- - Disable search highlight on next cursor moved.
+--- @return string # command to use expr mapping
 function searcho.normal(cmd)
   return require("searcho.command").normal(cmd)
 end
---
+++ spec/lua/searcho/example.lua
@@ -0,0 +1,21 @@
+vim.keymap.set({ "n", "x" }, "*", function()
+  local pattern = [[\v]] .. vim.fn.expand("<cword>")
+  return require("searcho").forward() .. pattern
+end, { expr = true })
+vim.keymap.set({ "n", "x" }, "#", function()
+  local pattern = [[\v]] .. vim.fn.expand("<cword>")
+  return require("searcho").backward() .. pattern
+end, { expr = true })
+
+vim.keymap.set({ "n", "x" }, "n", function()
+  return require("searcho").normal("n")
+end, { expr = true })
+vim.keymap.set({ "n", "x" }, "N", function()
+  return require("searcho").normal("N")
+end, { expr = true })
+
+require("searcho").setup_keymaps(function(vim)
+  vim.keymap.set("c", "<Tab>", [[<C-g>]], { buffer = true })
+  vim.keymap.set("c", "<S-Tab>", [[<C-t>]], { buffer = true })
+  vim.keymap.set("c", "<Space>", [[<CR>]], { buffer = true })
+end)
